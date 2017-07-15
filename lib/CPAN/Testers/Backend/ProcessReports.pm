ced735f5cd46c2ca38177ff1de226d1966034e29
Doug Bell <doug@preaction.me>
2017-07-15 17:02:37 -0500

    fix a little bit of POD spacing
    I'm now simply cargo-culting this, but if I recall correctly there used
    to be tools that could not handle closely-cuddled POD...

--- lib/CPAN/Testers/Backend/ProcessReports.pm
+++ lib/CPAN/Testers/Backend/ProcessReports.pm
@@ -3,6 +3,7 @@ our $VERSION = 0.001;
 # ABSTRACT: Process an incoming test report into useful statistics
 
 =head1 SYNOPSIS
+
     # container.yml     - A Beam::Wire container file
     process_reports:
         $class: CPAN::Testers::Backend::ProcessReport
@@ -13,13 +14,18 @@ our $VERSION = 0.001;
     $ beam run process_reports
     # Run with Beam::Minion
     $ beam minion run process_reports
+
 =head1 DESCRIPTION
+
 This module is a L<Beam::Runnable> task that reads incoming test reports
 from testers and produces the basic stats needed for the common
 reporting on the website and via e-mail. This is the first step in
 processing test data: All other tasks require this step to be completed.
+
 =head1 SEE ALSO
+
 L<CPAN::Testers::Backend>, L<CPAN::Testers::Schema>, L<Beam::Runnable>
+
 =cut
 
 use v5.24;
@@ -32,7 +38,9 @@ use Log::Any::Adapter 'Syslog';
 with 'Beam::Runnable';
 
 =attr schema
+
 A L<CPAN::Testers::Schema> object to access the database.
+
 =cut
 
 has schema => (
@@ -42,8 +50,10 @@ has schema => (
 );
 
 =attr cache
+
 A L<DBI> database handle for the metabase cache. This is temporary
 until all things can be told not to use the metabase cache.
+
 =cut
 
 has cache => (
@@ -54,8 +64,10 @@ has cache => (
 );
 
 =attr from
+
 An e-mail address to use when sending an error back to the tester that
 submitted a bad report.
+
 =cut
 
 has from => (
@@ -65,9 +77,11 @@ has from => (
 );
 
 =attr log
+
 To direct logs to a given place, use a L<Log::Any::Adapter> with
 C<lifecycle: eager>. By default, logs are routed to syslog using
 L<Log::Any::Adapter::Syslog>.
+
 =cut
 
 has _testers => (
@@ -78,8 +92,10 @@ has _testers => (
 );
 
 =method run
+
 The main method that processes job arguments and performs the task.
 Called by L<Beam::Runner> or L<Beam::Minion>.
+
 =cut
 
 sub run( $self, @args ) {
@@ -102,23 +118,20 @@ sub run( $self, @args ) {
 }
 
 =method find_unprocessed_reports
+
 Returns a list of L<CPAN::Testers::Schema::ResultSet::TestReport>
 objects for reports that are not in the cpanstats table.
+
 =cut
 
 sub find_unprocessed_reports( $self ) {
     my $schema = $self->schema;
     my $stats = $schema->resultset('Stats');
     my $reports = $schema->resultset('TestReport')->search({
-      id => { -not_in => $stats->get_column('guid')->as_query },
+        id => { -not_in => $stats->get_column('guid')->as_query },
     });
     return $reports->all;
 }
-
-#=method _send_email
-#
-#   $self->_send_email(
-#=cut
 
 1;
 
