1bb36aee743a096bf83bad7263f87587ed1a94c2
Doug Bell <doug@preaction.me>
2020-10-11 10:42:42 -0500

    add work-in-progress for replacement commands
    These will replace existing old CPAN testers scripts

---
+++ lib/CPAN/Testers/Backend/ProcessUploads.pm
@@ -0,0 +1,97 @@
+package CPAN::Testers::Backend::ProcessUploads;
+our $VERSION = '0.006';
+# ABSTRACT: Process CPAN uploads into database rows
+
+=head1 SYNOPSIS
+
+    beam run <container> <task>
+
+=head1 DESCRIPTION
+
+This module is a L<Beam::Runnable> task that scans the local CPAN mirror
+for new uploads and deleted files and updates the C<uploads> database
+table accordingly (via L<CPAN::Testers::Schema::Result::Upload>).
+
+This uses L<CPAN::DistnameInfo> to parse the CPAN file path into the
+distribution name, version, and author.
+
+=cut
+
+use v5.24;
+use warnings;
+use Moo;
+use experimental 'signatures', 'postderef';
+use Log::Any '$LOG';
+use Types::Standard qw( InstanceOf RegExp );
+use Types::Path::Tiny qw( AbsPath );
+with 'Beam::Runnable';
+use CPAN::DistnameInfo;
+
+=attr schema
+
+A L<CPAN::Testers::Schema> object to access the database.
+
+=cut
+
+has schema => (
+    is => 'ro',
+    isa => InstanceOf['CPAN::Testers::Schema'],
+    required => 1,
+);
+
+=attr cpan_root
+
+The path to the CPAN mirror root directory. Must be absolute.
+
+=cut
+
+has cpan_root => (
+    is => 'ro',
+    isa => AbsPath,
+    coerce => AbsPath->coercion,
+    required => 1,
+);
+
+=attr backpan_root
+
+The path to the BackPAN mirror root directory. Must be absolute.
+
+=cut
+
+has backpan_root => (
+    is => 'ro',
+    isa => AbsPath,
+    coerce => AbsPath->coercion,
+    required => 1,
+);
+
+=attr dist_ext
+
+The available extentions for a CPAN distribution. A regular expression.
+Defaults to allowing: C<.tar.gz>, C<.tar.bz2>, C<.tgz>, and C<.zip>.
+
+=cut
+
+has dist_ext => (
+    is => 'ro',
+    isa => RegExp,
+    default => qr{\.(tar\.(gz|bz2)|tgz|zip)$},
+);
+
+=method run
+
+The main method that processes job arguments and performs the task.
+Called by L<Beam::Runner> or L<Beam::Minion>.
+
+=cut
+
+sub run( $self, @args ) {
+
+    # Get the current list of CPAN uploads
+    # Add new uploads
+    # Mark missing uploads as BackPAN'd
+    # Update the ixlatest table?
+
+}
+
+1;
---
+++ lib/CPAN/Testers/Backend/UpdateReleaseSummary.pm
@@ -0,0 +1,52 @@
+package CPAN::Testers::Backend::UpdateReleaseSummary;
+our $VERSION = '0.006';
+# ABSTRACT: Update the release summary information
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This module is a L<Beam::Runnable> task that updates the release
+data (via L<CPAN::Testers::Schema::Result::Release>).
+
+=head1 SEE ALSO
+
+=cut
+
+use v5.24;
+use warnings;
+use Moo;
+use experimental 'signatures', 'postderef';
+use Log::Any '$LOG';
+use Types::Standard qw( InstanceOf );
+with 'Beam::Runnable';
+
+=attr schema
+
+A L<CPAN::Testers::Schema> object to access the database.
+
+=cut
+
+has schema => (
+    is => 'ro',
+    isa => InstanceOf['CPAN::Testers::Schema'],
+    required => 1,
+);
+
+=method run
+
+The main method that processes job arguments and performs the task.
+Called by L<Beam::Runner> or L<Beam::Minion>.
+
+=cut
+
+sub run( $self, @args ) {
+
+    # Get the max summary stat ID from the release data table
+    # Get the stat rows from the max summary ID to the current latest
+    # summary ID
+    # Update the release table
+
+}
+
+1;
