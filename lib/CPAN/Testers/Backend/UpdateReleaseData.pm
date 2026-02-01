1b1b6b1ccc31074f3305fe4bed6c4a0507095a15
Doug Bell <doug@preaction.me>
2026-02-01 12:04:30 -0600

    also update release_summary table

---
+++ lib/CPAN/Testers/Backend/UpdateReleaseData.pm
@@ -0,0 +1,91 @@
+package CPAN::Testers::Backend::UpdateReleaseData;
+our $VERSION = '0.006';
+# ABSTRACT: Update the release information
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This module is a L<Beam::Runnable> task that updates the release
+data (via L<CPAN::Testers::Schema::Result::ReleaseStat>).
+
+=head1 SEE ALSO
+
+=cut
+
+use v5.24;
+use warnings;
+use Moo;
+use experimental qw( signatures postderef );
+use Log::Any '$LOG';
+use Types::Standard qw( InstanceOf );
+with 'Beam::Runnable';
+
+=attr schema
+
+A L<CPAN::Testers::Schema> object to access the database.
+
+=cut
+
+has schema => (
+    is => 'ro',
+    isa => InstanceOf['CPAN::Testers::Schema'],
+    required => 1,
+);
+
+=method run
+
+The main method that processes job arguments and performs the task.
+Called by L<Beam::Runner> or L<Beam::Minion>.
+
+=cut
+
+sub run( $self, @args ) {
+    $LOG->info('Starting', { class => __PACKAGE__ });
+
+    # Get the max summary stat ID from the release data table
+    my $release_rs = $self->schema->resultset('ReleaseStat');
+    my $max_release_data_id = $release_rs->get_column('id')->max();
+
+    # Get the stat rows from the max summary ID to the current latest
+    my $stats_rs = $self->schema->resultset('Stats');
+    my $max_cpanstats_id = $stats_rs->get_column('id')->max();
+    $LOG->info('Updating release_data table from cpanstats', { max_release_data_id => $max_release_data_id, max_cpanstats_id => $max_cpanstats_id });
+
+    $stats_rs = $stats_rs->search({ id => { '>' => $max_release_data_id }}, { order_by => 'id', prefetch => 'upload'});
+    my $written = 0;
+    my $batch_size = 10000;
+    my @batch;
+    while (my $row = $stats_rs->next) {
+        my %data = $row->get_columns;
+        push @batch, {
+            %data{qw(dist version id guid uploadid)},
+            # pass/fail/na/unknown => 1
+            lc $data{state} => 1,
+            # distmat
+            distmat => $data{version} =~ /_/ || $data{version} =~ /-TRIAL$/ ? 2 : 1,
+            # oncpan
+            oncpan => !$row->upload->type || $row->upload->type ne 'backpan' ? 1 : 2,
+            # perlmat (devel or stable)
+            perlmat => $data{perl} =~ /^5.(7|9|[1-9][13579])/ ? 2 : 1,
+            # patched (pre-release)
+            patched => $data{perl} =~ /RC\d+|patch/ ? 2 : 1,
+        };
+        if (@batch >= $batch_size) {
+            $LOG->info('Writing batch', { batch_size => scalar @batch, batch_max_id => $batch[-1]{id} });
+            $release_rs->populate(\@batch);
+            $written += @batch;
+            @batch = ();
+        }
+    }
+
+    if (@batch) {
+        $LOG->info('Writing batch', { batch_size => scalar @batch, batch_max_id => $batch[-1]{id} });
+        $release_rs->populate(\@batch);
+        $written += @batch;
+    }
+
+    $LOG->info('Finished', { written => $written });
+}
+
+1;
--- lib/CPAN/Testers/Backend/UpdateReleaseSummary.pm
+++ lib/CPAN/Testers/Backend/UpdateReleaseSummary.pm
@@ -19,6 +19,7 @@ use Moo;
 use experimental qw( signatures postderef );
 use Log::Any '$LOG';
 use Types::Standard qw( InstanceOf );
+use Getopt::Long qw( GetOptionsFromArray );
 with 'Beam::Runnable';
 
 =attr schema
@@ -41,39 +42,45 @@ Called by L<Beam::Runner> or L<Beam::Minion>.
 =cut
 
 sub run( $self, @args ) {
-    $LOG->info('Starting', { class => __PACKAGE__ });
+    GetOptionsFromArray( \@args, \my %opt, 'clear' );
+    $LOG->info('Starting', { class => __PACKAGE__, clear => $opt{clear} });
 
-    # Get the max summary stat ID from the release data table
-    my $release_rs = $self->schema->resultset('Release');
-    my $max_release_data_id = $release_rs->get_column('id')->max();
+    my $data_rs = $self->schema->resultset('ReleaseStat');
+    my $summary_rs = $self->schema->resultset('Release');
 
-    # Get the stat rows from the max summary ID to the current latest
-    my $stats_rs = $self->schema->resultset('Stats');
-    my $max_cpanstats_id = $stats_rs->get_column('id')->max();
-    $LOG->info('Updating release_data table from cpanstats', { max_release_data_id => $max_release_data_id, max_cpanstats_id => $max_cpanstats_id });
+    my $from_id = 0;
+    if (!$opt{clear}) {
+      # Get the max summary stat ID from the release data table
+      my $max_data_id = $data_rs->get_column('id')->max();
+      my $max_summary_id = $summary_rs->get_column('id')->max();
 
-    $stats_rs = $stats_rs->search({ id => { '>' => $max_release_data_id }}, { order_by => 'id'});
-    my $written = 0;
-    my $batch_size = 1000;
-    my @batch;
-    while (my $row = $stats_rs->next) {
-        my %data = $row->get_columns;
-        push @batch, {
-            %data{qw(dist version id guid oncpan distmat perlmat patched uploadid)},
-            lc $data{state} => 1,
-        };
-        if (@batch >= $batch_size) {
-            $LOG->info('Writing batch', { batch_size => scalar @batch, batch_max_id => $batch[-1]{id} });
-            $release_rs->populate(\@batch);
-            $written += @batch;
-            @batch = ();
-        }
+      $LOG->info('Updating release_summary table from release_data', { max_data_id => $max_data_id, max_summary_id => $max_summary_id });
+      $from_id = $max_summary_id;
+    }
+    else {
+      $LOG->info('Rebuilding release_summary table from scratch');
     }
 
-    if (@batch) {
-        $LOG->info('Writing batch', { batch_size => scalar @batch, batch_max_id => $batch[-1]{id} });
-        $release_rs->populate(\@batch);
-        $written += @batch;
+    my @total_cols = qw( pass fail na unknown );
+    my $me = $data_rs->current_source_alias;
+    $data_rs = $data_rs->search( { id => { '>', $from_id }}, {
+        # The uploadid here is included to allow joins from the results
+        group_by => [ map "$me.$_", qw( dist version oncpan distmat perlmat patched uploadid ) ],
+        select => [
+            qw( dist version oncpan distmat perlmat patched uploadid ),
+            ( map { \"SUM($_) AS $_" } @total_cols ),
+            ( \sprintf 'SUM(%s) AS total', join ' + ', @total_cols )
+        ],
+        as => [ qw( dist version oncpan distmat perlmat patched uploadid ), @total_cols, 'total' ],
+        order_by => undef,
+    } );
+
+    my $written = 0;
+    while (my $row = $data_rs->next) {
+      my %row = $row->get_columns;
+      my $summary_row = $summary_rs->find_or_create({%row{qw(dist version oncpan distmat perlmat patched uploadid)}});
+      $summary_row->update({%row{qw(id guid pass fail na unknown)}});
+      $written++;
     }
 
     $LOG->info('Finished', { written => $written });
